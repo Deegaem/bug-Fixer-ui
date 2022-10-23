ed081173080059fce066814ba98dd520f30f4c82
elsadiq <sadig@gmx.ch>
2022-10-23 15:35:52 +0200

    changes:
    - restyled Bugdetails and Comment Components.

--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -17,6 +17,6 @@ import { AddEditCommentComponent } from './add-edit-comment/add-edit-comment.com
   imports: [
     SharedModule,
     CommentsRoutingModule
-  ], exports: [CommentsRoutingComponent, CommentsComponent]
+  ], exports: [CommentsRoutingComponent, CommentsComponent, AddEditCommentComponent]
 })
 export class CommentsModule { }
